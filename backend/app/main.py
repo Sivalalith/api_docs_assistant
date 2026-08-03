9db5aebf436f5ae993df3756d9d587feddf1cbb2
sivalalith <kspvsivalalith@gmail.com>
2026-08-04 01:01:32 +0530

    feat(api): configure frontend-backend communication, closes #3

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="API Documents Assistant")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
     return {
         "message": "API Documents Assistant Backend Running"
     }
+    
+@app.get("/health")
+def health():
+    return {"status": "Backend Connected! 🚀"}
