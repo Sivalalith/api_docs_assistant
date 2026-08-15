d3b9a07c08032157c4eeeee213f392724f2e3f39
sivalalith <kspvsivalalith@gmail.com>
2026-08-16 00:29:36 +0530

    feat(ai): add Qdrant Point (vector + payload) storage. #21

--- backend/app/ai/vector_store.py
+++ backend/app/ai/vector_store.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 from qdrant_client import QdrantClient
 
+from qdrant_client.models import PointStruct
+import uuid
+
 load_dotenv()
 
 class VectorStore:
@@ -14,3 +17,30 @@ class VectorStore:
             url=os.getenv("QDRANT_URL"),
             api_key=os.getenv("QDRANT_API_KEY"),
         )
+        
+    def add_documents(
+    self,
+    texts: list[str],
+    embeddings: list[list[float]],
+    metadata: list[dict],
+):
+        points = []
+
+        for text, embedding, meta in zip(texts, embeddings, metadata):
+            payload = {
+            "text": text,
+            **meta,
+            }
+
+            points.append(
+                PointStruct(
+                    id=str(uuid.uuid4()),
+                    vector=embedding,
+                    payload=payload,
+                )
+            )   
+
+        self.client.upsert(
+            collection_name=self.COLLECTION_NAME,
+            points=points,
+            )
