2c9bae2e782bf79acb0a48e5cba8a5594cfaa44f
sivalalith <kspvsivalalith@gmail.com>
2026-08-21 20:29:07 +0530

    refactor(api): change API_URL to use Render hosted one instead of localhost url.

--- frontend/src/services/queryService.js
+++ frontend/src/services/queryService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const askQuery = async (query) => {
   const response = await axios.post(`${API_URL}/query`, {
